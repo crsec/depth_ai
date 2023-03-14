fc2ac6988f6e5b38546f1812479545458178c025
老陈醋 <210776020@qq.com>
2023-03-14 13:49:38 +0800

    Update generate.ts

--- src/pages/api/generate.ts
+++ src/pages/api/generate.ts
@@ -30,6 +30,6 @@ export const post: APIRoute = async (context) => {
 
     return new Response(parseOpenAIStream(response))
   }else{
-    return new   Response(response)
+    return new   Response("AI:"+response)
   }
 }
